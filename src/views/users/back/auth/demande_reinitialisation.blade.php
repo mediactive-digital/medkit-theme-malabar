018177d0a35b1eeab9e471962b0a819eaf92cdce
mediactive-digital <ludo@iMac-de-mediactive-digital.local>
2019-09-30 17:11:48 +0200

    Mise a jour de la page de resiliation

--- src/views/users/back/auth/demande_reinitialisation.blade.php
+++ src/views/users/back/auth/demande_reinitialisation.blade.php
@@ -1,31 +1,33 @@
 @extends('medKitTheme::_layouts.back.notlogged')
 @section('content')
-
-<div class="col-md-10 col-lg-8 col-xl-6 mx-auto">
-    @if (session('status'))
-        <div class="alert alert-success mb-4">
-            {{ session('status') }}
+<section class="h-100">
+        <div class="row h-100">
+                <div class="col-md-10 align-self-center col-lg-8 col-xl-6 mx-auto">
+                        @if (session('status'))
+                            <div class="alert alert-success mb-4">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                    
+                        <div class="card">
+                            <div class="card-header">{{ _i('Réinitialiser le mot de passe') }}</div>
+                    
+                            <div class="card-body">
+                                <div class="alert alert-info">
+                                    {{ _i('Veuillez saisir votre adresse email pour réinitialiser votre mot de passe.') }}<br />
+                                    {{ _i('Vous recevrez les instructions dans quelques instants.') }}
+                                </div>
+                    
+                                {{-- <div class="row"> --}}
+                                    {{-- <div class="col-md-6 mx-auto"> --}}
+                                        {!! form($form) !!}
+                                    {{-- </div> --}}
+                                {{-- </div> --}}
+                            </div>
+                        </div>
+                    </div>
         </div>
-    @endif
+</section>
 
-    <div class="card">
-        <div class="card-header">{{ _i('Réinitialiser le mot de passe') }}</div>
-
-        <div class="card-body">
-            <div class="alert alert-info">
-                {{ _i('Veuillez saisir votre adresse email pour réinitialiser votre mot de passe.') }}<br />
-                {{ _i('Vous recevrez les instructions dans quelques instants.') }}
-            </div>
-
-            <div class="row">
-                <div class="col-md-6 mx-auto">
-                    {!! form($form) !!}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
-</html>
 
 @endsection
