ea3ec77005310b4c7e4b07901c5d0e681602adbc
mediactive-digital <ludo@iMac-de-mediactive-digital.local>
2019-10-03 14:20:17 +0200

    Last modif for theme

--- src/views/users/back/auth/reinitialisation.blade.php
+++ src/views/users/back/auth/reinitialisation.blade.php
@@ -1,22 +1,33 @@
 @extends('medKitTheme::_layouts.back.notlogged')
 @section('content')
 
-
-<div class="col-md-10 col-lg-8 col-xl-6 mx-auto">
-    <div class="card">
-        <div class="card-header">{{ _i('Réinitialiser le mot de passe') }}</div>
-
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-6 mx-auto">
-                    {!! form($form) !!}
+<section class="h-100">
+        <div class="row h-100">
+                <div id="bg_joli_a_mettre" class="bg-primary d-none d-lg-block col-lg-6 h-100 text-light p-0">
+                        <div class="d-flex h-100  justify-content-center">
+                            <div class="align-self-center">
+                                <p class="h1 mb-5">{{ _i('Réinitialiser le mot de passe') }} </p>
+                                {{-- <h1 class="bg-primary d-inline p-2 ">de votre mot de passe</h1> --}}
+                                {{-- <p class="mt-5 h2">avec une jolie  date<br> par exemple c'est pas mal non ?</p> --}}
+                            </div>
+                        </div>
                 </div>
-            </div>
+                <div class="col-lg-6 h-100">
+                        <div class="d-flex h-100  justify-content-center">
+                            <div class="align-self-center w-50 mx-auto">
+                            
+                                <div class="card">
+                                        <div class="card-header">{{ _i('Réinitialiser le mot de passe') }}</div>
+                                
+                                        <div class="card-body">
+                                                    {!! form($form) !!}
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        
+                    </div>
         </div>
-    </div>
-</div>
-
-</body>
-</html>
+</section>
 
 @endsection
