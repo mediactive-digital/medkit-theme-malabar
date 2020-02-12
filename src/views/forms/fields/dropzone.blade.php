5706dd88f37fb5ce781c365184bafefe7e8bf8cd
mediactive-digital <ludo@iMac-de-mediactive-digital.local>
2020-02-12 13:03:15 +0100

    Ajout fields dropzone

---
+++ src/views/forms/fields/dropzone.blade.php
@@ -0,0 +1,25 @@
+@if ($showLabel && $showField) 
+    @if ($options['wrapper'] !== false)
+        <div {!! $options['wrapperAttrs'] !!}>
+    @endif
+@endif
+
+@if ($showLabel && $options['label'] !== false && $options['label_show'])
+    {!! Form::customLabel($name, $options['label'], $options['label_attr']) !!}
+@endif
+
+@if ($showField)
+    {!! Form::input($type, $name, $options['value'], $options['attr']) !!}
+@endif
+
+@if($showError && isset($errors) && $errors->hasBag($errorBag))
+    @foreach($errors->getBag($errorBag)->get($nameKey) as $err)
+        <div {!! $options['errorAttrs'] !!}> {!! $err !!} </div>
+    @endforeach
+@endif
+
+@if ($showLabel && $showField) 
+    @if ($options['wrapper'] !== false)
+    </div>
+    @endif
+@endif
--- src/views/forms/fields/select2.blade.php
+++ src/views/forms/fields/select2.blade.php
@@ -23,8 +23,8 @@
 @if($showError && isset($errors) && $errors->hasBag($errorBag))
     @foreach($errors->getBag($errorBag)->get($nameKey) as $err)
         <div {!! $options['errorAttrs'] !!}> {!! $err !!} </div>
-        @endforeach
-        @endif
+    @endforeach
+@endif
 
 
         @if ($showLabel && $showField && $options['wrapper'] !== false)
