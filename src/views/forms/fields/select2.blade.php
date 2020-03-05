ae9e54f45abbfefa06803dd11fc45868fd71caea
Farand Chalal <f.chalal@mediactive.fr>
2020-03-05 16:23:34 +0100

    Fixes translatable / select2

--- src/views/forms/fields/select2.blade.php
+++ src/views/forms/fields/select2.blade.php
@@ -1,79 +1,110 @@
-@if($showLabel && $showField && $options['wrapper'] !== false)
-	<div {!! $options['wrapperAttrs'] !!}>
+@if ($showLabel && $showField && $options['wrapper'])
+    <div {!! $options['wrapperAttrs'] !!}>
 @endif
 
-@if ($showLabel && $options['label'] !== false && $options['label_show'])
-	{!! Form::customLabel($name, $options['label'], $options['label_attr']) !!}
+    @if ($showLabel && $options['label'] && $options['label_show'])
+        {!! Form::customLabel($name, $options['label'], $options['label_attr']) !!}
+    @endif
+
+    @if ($showField)
+        @php
+
+            $array = '';
+
+            if (isset($options['select2Opts']['multiple']) && $options['select2Opts']['multiple']) {
+
+                $options['attr']['multiple'] = 'multiple';
+                $options['attr']['id'] = $name;
+                $array = '[]';
+            }
+
+        @endphp
+
+        {!! Form::select($name . $array, $options['choices'], $options['value'], $options['attr']) !!}
+
+        @if ($options['help_block']['text'] && !$options['is_child'])
+            <{!! $options['help_block']['tag'] !!} {!! $options['help_block']['helpBlockAttrs'] !!}>
+                {!! $options['help_block']['text'] !!}
+            </ {!! $options['help_block']['tag'] !!}>
+        @endif
+    @endif
+
+    @if($showError && isset($errors) && $errors->hasBag($errorBag))
+        @foreach($errors->getBag($errorBag)->get($nameKey) as $err)
+            <div {!! $options['errorAttrs'] !!}>{!! $err !!}</div>
+        @endforeach
+    @endif
+
+@if ($showLabel && $showField && $options['wrapper'])
+    </div>
 @endif
 
-@if ($showField)
-	@php
-	if( isset($options['select2Opts']['multiple']) && $options['select2Opts']['multiple']){
-	$options['attr']['id'] = $name;
-	}
-	@endphp
-
-	{!! Form::select($name.(isset($options['select2Opts']['multiple']) && $options['select2Opts']['multiple'] ? '[]' : ''), $options['choices'],  $options['select2Opts']['multiple'] ? null : $options['value'], $options['attr']) !!}
-
-	@if ($options['help_block']['text'] && !$options['is_child'])
-		<{!! $options['help_block']['tag'] !!} {!! $options['help_block']['helpBlockAttrs'] !!}>
-		{!! $options['help_block']['text'] !!}
-		</ {!! $options['help_block']['tag'] !!}>
-	@endif
-@endif
-
-@if($showError && isset($errors) && $errors->hasBag($errorBag))
-	@foreach($errors->getBag($errorBag)->get($nameKey) as $err)
-		<div {!! $options['errorAttrs'] !!}> {!! $err !!} </div>
-	@endforeach
-@endif
-
-@if ($showLabel && $showField && $options['wrapper'] !== false)
-	</div>
-@endif
-<?php
- //dump( $options );
-?>
- 
 @push('scripts')
-<script>
-	$(document).ready(function () {
+    <script>
 
-		var element{{$name}}Select2 = $('#{{$name}}');
-		var opts{{$name}}Select2 = @json($options['select2Opts']);
+        @if (!isset($IS_ENABLED_SELECT2))
+            @php
 
-		@if (isset($options['customRenderSelect2']) && count($options['customRenderSelect2']) > 0)
-			var opts{{$name}}FromBackSelect2 = @json($options['customRenderSelect2']);
+                $IS_ENABLED_SELECT2 = true;
+                View::share('IS_ENABLED_SELECT2', $IS_ENABLED_SELECT2);
 
-			function render{{$name}}Select2options(state) {
-				if (state.text.indexOf('Searching') > -1) {
-					return state;
-				}
-				
-				var $state = opts{{$name}}FromBackSelect2.html;
-				// console.log('state', state)
-				// console.log('opts{{$name}}FromBackSelect2', opts{{$name}}FromBackSelect2)
+            @endphp
 
-				if ($state.indexOf('##ID##') > -1) {
-					$state = $state.replace('##ID##', state.id.toLowerCase());
-				}
+            function setSelect2Data(element, options) {
 
-				if ($state.indexOf('##TEXT##') > -1) {
-					$state = $state.replace('##TEXT##', state.text);
-				}
+                element.find('option').each(function() {
 
-				return $($state);
-			}
+                    var elementText = $(this).text();
+                    var tmp = document.createElement('div');
+                    tmp.innerHTML = elementText;
+                    $(this).text(tmp.lastChild.textContent || tmp.lastChild.innerText || '');
+                    tmp.remove();
 
-			opts{{$name}}Select2.templateResult = render{{$name}}Select2options;
-			// opts{{$name}}Select2.templateSelection = renderSelect2options;
-		@endif
+                    if (options.html) {
 
-		element{{$name}}Select2.select2(opts{{$name}}Select2);
-		
-		@if (isset($options['selected']))
-			element{{$name}}Select2.val([ {!! implode(',', $options['selected']) !!} ]).change();
-		@endif
-	});
-</script>
+                        var html = options.html;
+
+                        if (html.indexOf('##ID##') > -1) {
+
+                            html = html.replace(/##ID##/g, $(this).val());
+                        }
+
+                        if (html.indexOf('##TEXT##') > -1) {
+
+                            html = html.replace(/##TEXT##/g, elementText);
+                        }
+
+                        $(this).attr('data-html', html);
+                    }
+                });
+            };
+
+            function getSelect2TemplateResult(state) {
+
+                return (text = $(state.element).attr('data-html')) ? $(text) : state.text;
+            };
+        @endif
+
+        $(document).ready(function() {
+
+            var element = $('#{{ $name }}');
+            var options = @json($options['select2Opts']);  
+
+            @if (isset($options['customRenderSelect2']) && $options['customRenderSelect2'])
+
+                setSelect2Data(element, @json($options['customRenderSelect2']));
+                options.templateResult = getSelect2TemplateResult;
+
+            @endif
+
+            element.select2(options);
+
+            @if (isset($options['selected']))
+
+                element.val([{!! implode(',', $options['selected']) !!}]).change();
+
+            @endif
+        });
+
+    </script>
 @endpush
